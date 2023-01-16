eb5656281153b560c9a821e8fef105abcf2af9d0
ibraphem <ibraphem@gmail.com>
2023-01-16 10:33:33 +0100

    change to live URl

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:6500/api"
+// export const BASE_URL = "http://localhost:6500/api"
+export const BASE_URL = "https://wazobiaassesmentbackend-production.up.railway.app"
